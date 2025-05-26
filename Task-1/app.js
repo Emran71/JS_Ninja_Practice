ac32df691f50ce5b60cd02c60a7ba2954f924cb8
emran71 <ekhan4475@gmail.com>
2025-05-26 21:37:27 +0600

    Task-1 is completed

---
+++ Task-1/app.js
@@ -0,0 +1,47 @@
+//Exercise 1:
+let myName = "Emran Khan";
+let email = 'ekhan4475@gmail.com';
+let age =28;
+let isDeveloper =true;
+let goingToBeDesigner =null;
+
+//  Exercise 2:
+
+console.log("Hey it's me",myName,"I am" ,age, "years old. You can contact me by this", email,".I am a Developer and it's",isDeveloper,".I am going to be a Designer -", goingToBeDesigner);
+console.log(`Hey it's me <${myName}>. I am <${age}> years old. You can contact me by this <${email}>. I am a Developer and it's <${isDeveloper}>. I am going to be a Designer - ${goingToBeDesigner}
+`);
+
+// Exercise 3:
+var myname1="emran";
+var myname1 ="Abir"
+console.log(myname1);
+
+// let myname2 ="kabir"
+// myname2 = "rahim" 
+// let myname2 ="Amin"
+// console.log(myname2);
+
+// Explaination :we can easily redeclare variable and reassigned value in 'var'. In 'let' we can not redeclare variable but reassigned the value, so when
+// we want to redechare the variable it throws an error. And finally we can declare and assigned variable only one in 'const'. If we want to redeclare or 
+// reassigned variable it throws an error
+
+// Exercise 4:
+ let myname = undefined;
+ const ages = null;
+// In JS the null and undefined datatypes are primitive And their values also primitive. null is set by the user but undefined is set by the Javascript. 
+// but undefined set by the user is not good practice.
+
+//  Exercise 5:
+//  NaN is the abbroviation of Not a Number That means is not a number type. but it is actually a special numeric value in JavaScript that represents an
+//  invalid or unrepresentable number. It appears when a mathematical operation fails to produce a valid number. That means a user wants to add two
+//  number but one input is string. it throws NaN.And the type of NaN is number
+
+
+// Exercise 6:
+let thing;
+console.log(thing);
+
+// here the variable which has no value it returns undefined value because when the variable has no data javascript throws an undefined value. undefined
+// value means the variable has no value yet.
+
+
